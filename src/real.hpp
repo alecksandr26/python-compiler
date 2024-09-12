37268bf92518efdcb80d2f9a087150eb8fc4a103
Erick Carrillo <erick.carillo@toshibagcs.com>
2024-09-12 10:32:08 -0600

    The lexer is ready

--- src/real.hpp
+++ src/real.hpp
@@ -9,6 +9,14 @@ namespace pyc {
 		double value_;
 	public:
 		Real(const double &value, const Token &token);
+
+		
+		friend std::ostream &operator<<(std::ostream &os, const Real &real)
+		{
+			os << "Real<" << TokenType::token_types_str[real.get_type()]
+			   << ", \"" << real.value_ << "\">";
+			return os;
+		}
 	};
 }
 
