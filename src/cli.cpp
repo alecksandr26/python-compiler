92ff83fdf364d66e1b577a91119a014dfb2900d0
alecksandr <sansepiol26@gmail.com>
2024-10-05 10:25:55 -0600

    a basic cli created

--- src/cli.cpp
+++ src/cli.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <cstring>
 #include <cassert>
+#include <regex>
+
 
 struct {
 	std::string output_file, input_file;
@@ -18,13 +20,21 @@ int main(int argc, char *argv[])
 		std::string arg(argv[i]);
 
 		if (arg == "-o") {
-			assert(i + 1 < argc && "CLI: Error needs to have an output file");
+			assert(i + 1 < argc && "CLI: Error compiler needs to have an output file");
 			compiler_params.output_file = std::string(argv[i + 1]);
 			i++;	// Move to the next
 		} else {
-			
+			compiler_params.input_file = std::string(argv[i]);
+			std::regex pattern(R"(\.py$)");
+
+			assert(std::regex_search(compiler_params.input_file, pattern)
+			       && "CLI: Error compiler needs to have a valid input file");
 		}
 	}
+
+	assert(compiler_params.input_file != ""
+	       && "CLI: Error compiler needs to have an input file");
+	
 	
 	return 0;
 }
